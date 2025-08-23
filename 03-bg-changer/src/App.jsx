b114d2f31699b79c957860e92ee147ed39b02b92
Ashish singh <ashishsingh4857@gmail.com>
2025-08-23 10:43:24 +0530

    implemented bg-changer functionality

--- 03-bg-changer/src/App.jsx
+++ 03-bg-changer/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Buttons from "./components/buttons";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<div className="w-full h-screen bg-olive">
+				<div className="w-full bg-gray-500 rounded-full flex justify-center fixed bottom-12 justify-between p-2 ">
+					<Buttons color="red" />
+					<Buttons color="blue" />
+					<Buttons color="red" />
+					<Buttons color="pink" />
+					<Buttons color="purple" />
+					<Buttons color="red" />
+					<Buttons color="red" />
+					<Buttons color="red" />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default App;
