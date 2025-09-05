6ea2614cb14bbc0438ab2b9aa7652c3b7e7c3c40
Ashish singh <ashishsingh4857@gmail.com>
2025-09-05 16:27:44 +0530

    feat:implemented browser routing

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+
+				<Route
+					path="*"
+					element={
+						<h1 className="text-center mt-20">
+							404 Not Found
+						</h1>
+					}
+				/>
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
