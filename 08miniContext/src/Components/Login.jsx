686aec1ba83d9b77d0401bf3653483e2802a4410
Ashish singh <ashishsingh4857@gmail.com>
2025-09-06 17:23:18 +0530

    feat: added context fun

--- 08miniContext/src/Components/Login.jsx
+++ 08miniContext/src/Components/Login.jsx
@@ -1,7 +1,46 @@
-import React from "react";
-
+import react, { useContext, useState } from "react";
+import UserContext from "../context/UserContext";
 const Login = () => {
-	return <div>Login</div>;
+	const { user, setUser } = useContext(UserContext);
+	const [Username, SetUsername] = useState("");
+	const [password, SetPassword] = useState("");
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setUser(Username);
+	};
+
+	return (
+		<>
+			<div className="border p-4 w-1/4 mx-auto my-5">
+				<h1 className="text-xl font-bold text-center">Login</h1>
+				<form onSubmit={handleSubmit}>
+					<input
+						className="border p-1 m-1 w-full"
+						type="text"
+						placeholder="Username"
+						onChange={(e) =>
+							SetUsername(e.target.value)
+						}
+						value={Username}
+					/>
+					<input
+						className="border p-1 m-1 w-full"
+						type="password"
+						placeholder="Password"
+						onChange={(e) =>
+							SetPassword(e.target.value)
+						}
+						value={password}
+					/>
+					<button
+						className="border rounded-2xl p-1 m-1 w-full bg-blue-500 text-white"
+						type="submit">
+						Login
+					</button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Login;
--- 08miniContext/src/Components/Profile.jsx
+++ 08miniContext/src/Components/Profile.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 function Profile() {
-	return <div>Profile</div>;
+	const { user } = useContext(UserContext);
+
+	return (
+		<>
+			<div className="border p-4 w-1/4 mx-auto my-5">
+				<h1 className="text-xl font-bold text-center">
+					Profile
+				</h1>
+				<p className="text-center">
+					Welcome, {user ? user : "admin"}
+				</p>
+			</div>
+		</>
+	);
 }
 
 export default Profile;
