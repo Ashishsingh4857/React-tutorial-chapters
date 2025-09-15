d4e0604c509d88f5068f9d6a84cc65245227cf10
Ashish singh <ashishsingh4857@gmail.com>
2025-09-15 13:02:02 +0530

    feat:added index.js for component imports

---
+++ appWrite-Blog/src/components/index.js
@@ -0,0 +1,28 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./select";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
+import Signup from "./Signup";
+import RTE from "./RTE";
+import PostCard from "./PostCard";
+
+export {
+	Header,
+	Footer,
+	Container,
+	LogoutBtn,
+	Input,
+	Button,
+	Select,
+	Logo,
+	Login,
+	Signup,
+	RTE,
+	PostCard,
+};
